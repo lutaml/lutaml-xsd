eb9358e582ef152794d96982a809d1917e2a83a7
suleman-uzair <suleman@workytical.com>
2024-11-18 19:02:59 +0500

    chore: removed zeitwerk default logger

--- lib/lutaml/xsd.rb
+++ lib/lutaml/xsd.rb
@@ -4,7 +4,6 @@ require "zeitwerk"
 require "lutaml/model"
 require_relative "xsd/xsd"
 
-Zeitwerk::Loader.default_logger = method(:puts)
 loader = Zeitwerk::Loader.for_gem(warn_on_extra_files: true)
 loader.push_dir("lib/lutaml/xsd", namespace: Lutaml::Xsd)
 loader.ignore("#{__dir__}/lib/lutaml/xsd.rb")
