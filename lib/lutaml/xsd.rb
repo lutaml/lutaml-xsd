192cfdf397ab1cd34689caef58a1a5c35683a2c9
suleman-uzair <suleman@workytical.com>
2024-11-20 13:55:34 +0500

    chore: updated dir path string

--- lib/lutaml/xsd.rb
+++ lib/lutaml/xsd.rb
@@ -5,6 +5,6 @@ require "lutaml/model"
 require_relative "xsd/xsd"
 
 loader = Zeitwerk::Loader.for_gem(warn_on_extra_files: true)
-loader.push_dir("lib/lutaml/xsd", namespace: Lutaml::Xsd)
+loader.push_dir("#{__dir__}/xsd", namespace: Lutaml::Xsd)
 loader.ignore("#{__dir__}/lib/lutaml/xsd.rb")
 loader.setup
