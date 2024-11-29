7cc1d43431529866833697183896bdc3a1902192
suleman-uzair <suleman@workytical.com>
2024-11-29 20:15:41 +0500

    Added/Updated attributes

--- lib/lutaml/xsd/choice.rb
+++ lib/lutaml/xsd/choice.rb
@@ -8,9 +8,10 @@ module Lutaml
       attribute :any, Any, collection: true
       attribute :min_occurs, :string
       attribute :max_occurs, :string
+      attribute :group, Group, collection: true
+      attribute :choice, Choice, collection: true
       attribute :element, Element, collection: true
       attribute :sequence, Sequence, collection: true
-      attribute :group, Group, collection: true
 
       xml do
         root "choice", mixed: true
@@ -18,8 +19,9 @@ module Lutaml
 
         map_attribute :minOccurs, to: :min_occurs
         map_attribute :maxOccurs, to: :max_occurs
-        map_element :element, to: :element
         map_element :sequence, to: :sequence
+        map_element :element, to: :element
+        map_element :choice, to: :choice
         map_element :group, to: :group
         map_element :any, to: :any
       end
--- lib/lutaml/xsd/complex_content.rb
+++ lib/lutaml/xsd/complex_content.rb
@@ -3,13 +3,19 @@
 module Lutaml
   module Xsd
     class ComplexContent < Lutaml::Model::Serializable
+      attribute :id, :string
+      attribute :mixed, :boolean
       attribute :extension, Extension, collection: true
+      attribute :restriction, Restriction, collection: true
 
       xml do
         root "complexContent", mixed: true
         namespace "http://www.w3.org/2001/XMLSchema", "xsd"
 
+        map_attribute :id, to: :id
+        map_attribute :mixed, to: :mixed
         map_element :extension, to: :extension
+        map_element :restriction, to: :restriction
       end
     end
   end
