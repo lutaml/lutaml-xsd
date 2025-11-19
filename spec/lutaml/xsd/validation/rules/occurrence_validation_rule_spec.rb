6435ba4505d37e933e4c72f2dc36f4ae8aa546bf
Ronald Tse <ronald.tse@ribose.com>
2025-11-19 20:09:39 +0800

    fix: specs

--- spec/lutaml/xsd/validation/rules/occurrence_validation_rule_spec.rb
+++ spec/lutaml/xsd/validation/rules/occurrence_validation_rule_spec.rb
@@ -1,37 +1,28 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "lutaml/xsd"
 require "lutaml/xsd/validation/rules/occurrence_validation_rule"
 require "lutaml/xsd/validation/result_collector"
 require "lutaml/xsd/validation/validation_configuration"
 require "lutaml/xsd/validation/xml_element"
 require "lutaml/xsd/validation/xml_navigator"
-require "lutaml/xsd/element"
-require "lutaml/xsd/sequence"
 
 RSpec.describe Lutaml::Xsd::Validation::Rules::OccurrenceValidationRule do
   let(:config) { Lutaml::Xsd::Validation::ValidationConfiguration.new }
   let(:collector) { Lutaml::Xsd::Validation::ResultCollector.new(config) }
   let(:rule) { described_class.new }
 
-  # Helper to create mock schema element that passes case statement checks
-  def mock_schema_element(name:, min_occurs:, max_occurs:, target_namespace: nil)
-    Object.new.tap do |obj|
-      obj.define_singleton_method(:name) { name }
-      obj.define_singleton_method(:min_occurs) { min_occurs }
-      obj.define_singleton_method(:max_occurs) { max_occurs }
-      obj.define_singleton_method(:target_namespace) { target_namespace }
-      obj.define_singleton_method(:is_a?) do |klass|
-        klass == Lutaml::Xsd::Element || super(klass)
-      end
-      obj.define_singleton_method(:kind_of?) do |klass|
-        klass == Lutaml::Xsd::Element || super(klass)
-      end
-      obj.define_singleton_method(:instance_of?) do |klass|
-        klass == Lutaml::Xsd::Element || super(klass)
-      end
-      obj.define_singleton_method(:class) { Lutaml::Xsd::Element }
-    end
+  # Helper to create a moxml element with children
+  def create_moxml_element(name, children = [])
+    Struct.new(:name, :children, :namespace).new(name, children, nil)
+  end
+
+  # Helper to create a moxml child element
+  def create_moxml_child(name)
+    child = Struct.new(:name, :namespace).new(name, nil)
+    child.define_singleton_method(:element?) { true }
+    child
   end
 
   describe "#category" do
@@ -48,12 +39,14 @@ RSpec.describe Lutaml::Xsd::Validation::Rules::OccurrenceValidationRule do
   end
 
   describe "#validate" do
-    let(:navigator) { instance_double(Lutaml::Xsd::Validation::XmlNavigator, current_xpath: "/root") }
+    let(:navigator) do
+      Object.new.tap do |nav|
+        nav.define_singleton_method(:current_xpath) { "/root" }
+      end
+    end
 
     context "when schema particle is nil" do
-      let(:moxml_element) do
-        Struct.new(:name, :children).new("parent", [])
-      end
+      let(:moxml_element) { create_moxml_element("parent", []) }
       let(:xml_element) { Lutaml::Xsd::Validation::XmlElement.new(moxml_element, navigator) }
 
       it "does not validate" do
@@ -64,24 +57,22 @@ RSpec.describe Lutaml::Xsd::Validation::Rules::OccurrenceValidationRule do
     end
 
     context "with element occurrence constraints" do
-      let(:child_moxml1) do
-        obj = Struct.new(:name, :namespace).new("child", nil)
-        obj.define_singleton_method(:element?) { true }
-        obj
-      end
-      let(:child_moxml2) do
-        obj = Struct.new(:name, :namespace).new("child", nil)
-        obj.define_singleton_method(:element?) { true }
-        obj
-      end
-      let(:moxml_children) { [child_moxml1, child_moxml2] }
-      let(:moxml_element) do
-        Struct.new(:name, :children, :namespace).new("parent", moxml_children, nil)
-      end
-      let(:xml_element) { Lutaml::Xsd::Validation::XmlElement.new(moxml_element, navigator) }
-
       context "when minOccurs is satisfied" do
-        let(:schema_element) { mock_schema_element(name: "child", min_occurs: "1", max_occurs: "unbounded") }
+        let(:xsd_xml) do
+          <<~XSD
+            <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
+              <xs:element name="child" type="xs:string" minOccurs="1" maxOccurs="unbounded"/>
+            </xs:schema>
+          XSD
+        end
+
+        let(:child1) { create_moxml_child("child") }
+        let(:child2) { create_moxml_child("child") }
+        let(:moxml_element) { create_moxml_element("parent", [child1, child2]) }
+        let(:xml_element) { Lutaml::Xsd::Validation::XmlElement.new(moxml_element, navigator) }
+
+        let(:schema) { Lutaml::Xsd.parse(xsd_xml) }
+        let(:schema_element) { schema.element.first }
 
         it "does not report min occurs error" do
           rule.validate(xml_element, schema_element, collector)
@@ -92,16 +83,19 @@ RSpec.describe Lutaml::Xsd::Validation::Rules::OccurrenceValidationRule do
       end
 
       context "when minOccurs is violated" do
-        let(:moxml_child) do
-          obj = Struct.new(:name, :namespace).new("child", nil)
-          obj.define_singleton_method(:element?) { true }
-          obj
+        let(:xsd_xml) do
+          <<~XSD
+            <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
+              <xs:element name="child" type="xs:string" minOccurs="2" maxOccurs="unbounded"/>
+            </xs:schema>
+          XSD
         end
-        let(:moxml_element) do
-          Struct.new(:name, :children, :namespace).new("parent", [], nil)
-        end
+
+        let(:moxml_element) { create_moxml_element("parent", []) }
         let(:xml_element_empty) { Lutaml::Xsd::Validation::XmlElement.new(moxml_element, navigator) }
-        let(:schema_element) { mock_schema_element(name: "child", min_occurs: "2", max_occurs: "unbounded") }
+
+        let(:schema) { Lutaml::Xsd.parse(xsd_xml) }
+        let(:schema_element) { schema.element.first }
 
         it "reports min occurs violation" do
           rule.validate(xml_element_empty, schema_element, collector)
@@ -121,7 +115,21 @@ RSpec.describe Lutaml::Xsd::Validation::Rules::OccurrenceValidationRule do
       end
 
       context "when maxOccurs is violated" do
-        let(:schema_element) { mock_schema_element(name: "child", min_occurs: "1", max_occurs: "1") }
+        let(:xsd_xml) do
+          <<~XSD
+            <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
+              <xs:element name="child" type="xs:string" minOccurs="1" maxOccurs="1"/>
+            </xs:schema>
+          XSD
+        end
+
+        let(:child1) { create_moxml_child("child") }
+        let(:child2) { create_moxml_child("child") }
+        let(:moxml_element) { create_moxml_element("parent", [child1, child2]) }
+        let(:xml_element) { Lutaml::Xsd::Validation::XmlElement.new(moxml_element, navigator) }
+
+        let(:schema) { Lutaml::Xsd.parse(xsd_xml) }
+        let(:schema_element) { schema.element.first }
 
         it "reports max occurs violation" do
           rule.validate(xml_element, schema_element, collector)
@@ -141,18 +149,22 @@ RSpec.describe Lutaml::Xsd::Validation::Rules::OccurrenceValidationRule do
       end
 
       context "when maxOccurs is unbounded" do
+        let(:xsd_xml) do
+          <<~XSD
+            <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
+              <xs:element name="child" type="xs:string" minOccurs="1" maxOccurs="unbounded"/>
+            </xs:schema>
+          XSD
+        end
+
         let(:many_children) do
-          Array.new(100) do
-            obj = Struct.new(:name, :namespace).new("child", nil)
-            obj.define_singleton_method(:element?) { true }
-            obj
-          end
+          Array.new(100) { create_moxml_child("child") }
         end
-        let(:moxml_element_many) do
-          Struct.new(:name, :children, :namespace).new("parent", many_children, nil)
-        end
+        let(:moxml_element_many) { create_moxml_element("parent", many_children) }
         let(:xml_element_many) { Lutaml::Xsd::Validation::XmlElement.new(moxml_element_many, navigator) }
-        let(:schema_element) { mock_schema_element(name: "child", min_occurs: "1", max_occurs: "unbounded") }
+
+        let(:schema) { Lutaml::Xsd.parse(xsd_xml) }
+        let(:schema_element) { schema.element.first }
 
         it "does not report max occurs error" do
           rule.validate(xml_element_many, schema_element, collector)
