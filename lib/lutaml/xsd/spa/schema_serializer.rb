884a8019a44911c9814e2727fecd4190f3a6eddc
KW Kwan <kw.kwan@ribose.com>
2026-04-16 14:07:19 +0800

    Show correct type and default value when the attribute is an enumeration

--- lib/lutaml/xsd/spa/schema_serializer.rb
+++ lib/lutaml/xsd/spa/schema_serializer.rb
@@ -472,6 +472,75 @@ module Lutaml
           end.sort_by { |ag| ag[:name] || "" }
         end
 
+        # Extract enumeration default value from an attribute with inline simpleType
+        #
+        # @param attr [Attribute] Attribute object
+        # @return [String, nil] Formatted enumeration default string
+        def extract_enumeration_default(attr)
+          return [nil, nil] unless attr.respond_to?(:simple_type) && attr.simple_type
+
+          st = attr.simple_type
+          if st.respond_to?(:restriction) && st.restriction
+            # <xsd:simpleType>
+            #   <xsd:restriction base="xsd:token">
+            #     <xsd:enumeration value="A"/>
+            #     <xsd:enumeration value="B"/>
+            #   </xsd:restriction>
+            # </xsd:simpleType>
+            restriction = st.restriction
+            if restriction.respond_to?(:enumeration) && restriction.enumeration
+              enum_str = extract_enumeration_values(restriction.enumeration)
+              return [enum_str, restriction.base]
+            end
+          elsif st.respond_to?(:union) && st.union
+            # <xsd:simpleType>
+            #   <xsd:union memberTypes="xsd:token">
+            #     <xsd:simpleType>
+            #       <xsd:restriction base="xsd:token">
+            #         <xsd:enumeration value="A"/>
+            #         <xsd:enumeration value="B"/>
+            #       </xsd:restriction>
+            #     </xsd:simpleType>
+            #   </xsd:union>
+            # </xsd:simpleType>
+            union = st.union
+            union_type_str = ""
+            if union.respond_to?(:member_types)
+              union_type_str = union.member_types
+            end
+
+            if union.respond_to?(:simple_type) && union.simple_type
+              union_sub_type = union.simple_type.first
+              if union_sub_type.respond_to?(:restriction) &&
+                  union_sub_type.restriction
+                union_restriction = union_sub_type.restriction
+                if union_restriction.respond_to?(:enumeration) &&
+                    union_restriction.enumeration
+                  enum_val_str = extract_enumeration_values(union_restriction
+                    .enumeration)
+
+                  enum_type_str = union_restriction.base
+                  enum_str = "union of: [ #{union_type_str}, " \
+                             "[ #{union_restriction.base} #{enum_val_str} ] ]"
+
+                  return [enum_str, enum_type_str]
+                end
+              end
+            end
+          end
+
+          [nil, nil]
+        end
+
+        # Extract enumeration values in string
+        #
+        # @param attr [Enumeration] Enumeration object
+        # @return [String]
+        def extract_enumeration_values(enumeration)
+          values = enumeration.map { |e| "'#{e.value}'" }
+          "(value comes from list: #{values.join('|')})"
+        end
+
         # Serialize attributes from an attribute group
         #
         # @param ag [AttributeGroup] Attribute group
@@ -480,11 +549,13 @@ module Lutaml
           return [] unless ag.respond_to?(:attribute) && ag.attribute
 
           ag.attribute.map do |attr|
+            # Check for inline simpleType with enumeration
+            enum_default, enum_type = extract_enumeration_default(attr)
             {
-              name: attr.name,
-              type: attr.type,
+              name: attr.name || attr.ref,
+              type: enum_type || attr.type,
               use: attr.respond_to?(:use) ? attr.use : nil,
-              default: attr.respond_to?(:default) ? attr.default : nil,
+              default: enum_default || (attr.respond_to?(:default) ? attr.default : nil),
               fixed: attr.respond_to?(:fixed) ? attr.fixed : nil,
               documentation: extract_documentation(attr),
             }
@@ -823,11 +894,13 @@ module Lutaml
           return [] unless type.respond_to?(:attributes)
 
           type.attributes.map do |attr|
+            # Check for inline simpleType with enumeration
+            enum_default, enum_type = extract_enumeration_default(attr)
             {
-              name: attr.name,
-              type: attr.type,
+              name: attr.name || attr.ref,
+              type: enum_type || attr.type,
               use: attr.use,
-              default: attr.respond_to?(:default) ? attr.default : nil,
+              default: enum_default || (attr.respond_to?(:default) ? attr.default : nil),
               fixed: attr.respond_to?(:fixed) ? attr.fixed : nil,
               documentation: extract_documentation(attr),
             }
